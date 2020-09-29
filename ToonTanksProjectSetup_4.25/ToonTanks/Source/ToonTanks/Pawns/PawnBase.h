1895abf12c3226287cca4e0e72e4bcb0de9b6c45
William Carolin <wcarolin@Williams-MBP.T-mobile.com>
2020-09-29 17:46:11 -0400

    Added GameStart BluePrint widget.

--- ToonTanksProjectSetup_4.25/ToonTanks/Source/ToonTanks/Pawns/PawnBase.h
+++ ToonTanksProjectSetup_4.25/ToonTanks/Source/ToonTanks/Pawns/PawnBase.h
@@ -18,13 +18,13 @@ public:
 	// Sets default values for this pawn's properties
 	APawnBase();
 
+	virtual void HandleDestruction();
+
 protected:
 
 	void RotateTurret(FVector LookAtTarget);
 
 	void Fire();
-
-	virtual void HandleDestruction();
 
 
 private:
